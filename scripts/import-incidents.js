885533ac02c193b894ffb5d1ac4ae8f90e7a46a0
Cameron Sampson <vaerros@gmail.com>
2013-12-01 21:14:08 -0600

    Import script overhaul
    Addresses a major issue with the import script -
    crimemapping.com has a cap on their api response, so we'll instead
    build records day-by-day

--- scripts/import-incidents.js
+++ scripts/import-incidents.js
@@ -1,10 +1,12 @@
 var async = require('async'),
-    mongoose = require('mongoose'),
     http = require('http'),
     querystring = require('querystring'),
+    mongoose = require('mongoose'),
     moment = require('moment'),
     proj4 = require('proj4js'),
     Incident = require('../models/incident.js');
+
+require('twix');
 
 mongoose.connect('mongodb://localhost/nola_crime_stats');
 
@@ -26,24 +28,11 @@ function saveIncident(incident, callback) {
   incident.save(callback);
 }
 
-Incident.remove(function() {
-  console.log('Cleared incidents collection');
-  console.log('Fetching incident data from crimemapping.com');
+function fetchIncident(day, params, callback) {
+  params.db = day+'+00:00:00';
+  params.de = day+'+23:59:00';
 
-  var endDate = moment(),
-      beginningDate = moment(endDate).subtract('months', 6); // expect crime data to only be for 6 months back
-
-  var params = {
-    db: beginningDate.format('MM/DD/YYYY'), // beginning date
-    de: endDate.format('MM/DD/YYYY'), // end date
-    ccs: 'AR,AS,BU,DP,DR,DU,FR,HO,VT,RO,SX,TH,VA,VB,WE', // all crime types
-    xmin: -10058512.647324989, // This and other points use Google maps spatial ref. This is a rough square-plot of the New Orleans area
-    xmax: -9997363.02469693,
-    ymin: 3487932.83757992,
-    ymax: 3506468.8169390503
-  };
-
-  http.get('http://www.crimemapping.com/GetIncidents.aspx?' + querystring.stringify(params), function(response) {
+  http.get('http://www.crimemapping.com/GetIncidents.aspx?' + querystring.unescape(querystring.stringify(params)), function(response) {
     var body = '';
 
     response.on('data', function(chunk) {
@@ -51,12 +40,58 @@ Incident.remove(function() {
     });
 
     response.on('end', function() {
-      var parsedIncidents = JSON.parse(body).incidents;
+      var parsedBody = JSON.parse(body);
 
-      async.forEach(parsedIncidents, saveIncident, function() {
-        console.log('Finished inserting new incidents');
-        process.exit();
-      });
+      if('errorMessage' in parsedBody) {
+        console.log('Came back with an error: ' + parsedBody.errorMessage);
+        callback();
+      }
+      else if(!parsedBody.incidents.length) {
+        console.log('No incidents for ' + day);
+        callback();
+      }
+      else {
+        var parsedIncidents = parsedBody.incidents;
+
+        async.forEach(parsedIncidents, saveIncident, function() {
+          console.log('Finished inserting new incidents for ' + day);
+          callback();
+        });
+      }
     });
   });
+}
+
+Incident.remove(function() {
+  console.log('Cleared incidents collection');
+  console.log('Fetching incident data from crimemapping.com');
+
+  var now = moment(),
+      sixMonthsAgo = moment().subtract('month', 6), // expect crime data to only be available roughly 6 months back
+      dayIterator = sixMonthsAgo.twix(now).iterate('days');
+
+  var params = {
+    ccs: 'AR,AS,BU,DP,DR,DU,FR,HO,VT,RO,SX,TH,VA,VB,WE', // all crime type codes
+    xmin: -10058512.647324989, // These are points for a rough square-plot of New Orleans
+    xmax: -9997363.02469693,
+    ymin: 3487932.83757992,
+    ymax: 3506468.8169390503
+  };
+
+  async.whilst(
+    function() {
+      return dayIterator.hasNext();
+    },
+    function(callback) {
+      var day = dayIterator.next().format('MM/DD/YYYY');
+
+      console.log('Fetching incidents for ' + day);
+
+      fetchIncident(day, params, callback);
+    },
+    function() {
+      console.log('\nDone');
+      process.exit();
+    }
+  );
 });
