eea1f1245608b3afbbea8656e27ce1d52b00a5ca
Cameron Sampson <vaerros@gmail.com>
2013-12-29 20:49:18 -0600

    Grunt-karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,31 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
+      options: {
+        quiet: true
+      },
       dist: {
         files: {
           'public/css/app.css': 'public/css/app.scss'
         }
       }
     },
+    karma: {
+      unit: {
+        configFile: 'test/karma.conf.js',
+        tasks: ['karma:unit:run'],
+        background: true
+      }
+    },
     watch: {
-      files: '**/*.scss',
-      tasks: ['sass']
+      files: ['**/*.scss', 'public/js/**/*.js', 'test/unit/*.js'],
+      tasks: ['sass', 'karma:unit:run']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
+
   grunt.registerTask('default',['watch']);
 };
