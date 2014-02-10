e6ea0ca8d44dea1af858b1e94c4925d4834629bc
Cameron Sampson <vaerros@gmail.com>
2014-02-10 07:04:22 -0600

    Removed northing offset from import-incidents projection ref
    This reflects an apparent change in the data source -- they no
    longer appear to have offsets.

--- scripts/import-incidents.js
+++ scripts/import-incidents.js
@@ -11,7 +11,7 @@ require('twix');
 mongoose.connect('mongodb://localhost/crime_crunch');
 
 function saveIncident(incident, callback) {
-  var originalProjection = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=21350 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', // SR-ORG:6 with rough northing offset
+  var originalProjection = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', // SR-ORG:6
       newProjection = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', // WGS84
       coordinates = proj4(originalProjection, newProjection, [incident.Y, incident.X]);
 
